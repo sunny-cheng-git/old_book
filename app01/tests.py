38174e037b7a6da2398319c692ef62b9fb364f09
sunny <sunny@qq.com>
2020-05-25 17:02:24 +0800

    2次提交，完善一些文件

--- app01/tests.py
+++ app01/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
-dic = {'key':'value'}
-d = dic.get('key')
-print(d)
+# dic = {'key':'value'}
+# d = dic.get('key')
+# print(d)
+
+
+import re
+dd = re.search('media','media/book_imgs/英莲.jpg')
+print(dd.group())
